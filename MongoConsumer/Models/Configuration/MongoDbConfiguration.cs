2534760c0a93b981bcdef81dd8c9f9b110cda4a5
jonathan kalush <jonathan.kalush@gmail.com>
2026-03-07 18:08:33 +0200

    [minor] config

---
+++ MongoConsumer/Models/Configuration/MongoDbConfiguration.cs
@@ -0,0 +1,7 @@
+namespace MongoConsumer.Models.Configuration;
+
+public class MongoDbConfiguration
+{
+    public string ConnectionString { get; set; } = string.Empty;
+    public string DatabaseName { get; set; } = string.Empty;
+}
